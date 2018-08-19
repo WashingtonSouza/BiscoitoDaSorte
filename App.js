34b913b71ebed3951482cc20f97e62f821625c97
Washington Souza <thewashington.souza@gmail.com>
2018-08-19 17:48:48 -0300

    Implementação do botão

--- App.js
+++ App.js
@@ -1,15 +1,48 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+class Botao extends Component{
+
+   constructor(props){
+      super(props);
+      this.state = {};
+      this.style = StyleSheet.create({
+         botao:{
+            width:250,
+            height:50,
+            borderWidth:2,
+            borderColor:'#FF0000',
+            backgroundColor:'transparent',
+            borderRadius:25
+         }
+      });
+   }
+
+   render(){
+      return(
+         <TouchableOpacity style={this.style.Botao}>
+            <View>
+               <Text>Quebrar Biscoito</Text>
+            </View>
+         </TouchableOpacity>
+      );
+   }
+
+}
 
 export default class App extends Component {
+
   render() {
     return (
-      <View>        
+      <View style={styles.body}>
+         <Botao/>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  
+   body:{
+      paddingTop:20
+   }
 });
